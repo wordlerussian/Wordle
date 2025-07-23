fbad2b100d7c281270abb09fdffea3a623e98d33
wordlerussian <yaneznayu92@gmail.com>
2025-07-23 13:08:43 +0500

    💡 Add new features and updates

--- run.py
+++ run.py
@@ -13,7 +13,6 @@ async def main():
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
